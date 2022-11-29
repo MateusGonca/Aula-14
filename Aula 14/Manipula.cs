affaf994d2f63d3a63e5f61ecd784b435ef7fd8b
curso.ads2 <curso.ads2@LAB06-05>
2022-11-28 22:20:27 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 14/Manipula.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14
+{
+    public class Manipula
+    {
+        private string caminho =
+            "C://Users//aluno//Downloads/Dados.txt";
+
+        public void Gravar(string texto)
+        {
+            StreamWriter sw = new StreamWriter(caminho);
+            sw.Write(texto);
+            sw.Close();
+        }
+        public List<Aluno> Ler()
+        {
+
+            var list = new List<Aluno>();
+            if (File.Exists(caminho))
+            {
+
+                StreamReader sr = File.OpenText(caminho);
+                string linha;
+                while((linha = sr.ReadLine()) != null)
+                {
+                    var arquivo = linha.Split(';');
+                    Aluno alguem = new Aluno(int.Parse(arquivo[0]), arquivo[1], arquivo[2], arquivo[3]);
+
+                    list.Add(alguem);
+                }
+                sr.Close();
+            }
+            
+            return list;
+        }
+        public void Alterar(Aluno a)
+        {
+            string texto = null;
+            if(File.Exists(caminho))
+            {
+                using(StreamReader sr = new StreamReader(caminho))
+                {
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        var arquivo = linha.Split(';');
+                        if (int.Parse(arquivo[0]) == a.RA)
+                        {
+                            string nova = a.RA + ";" + a.Nome + ";" +
+                            a.Turma + ";" + a.CPF + "\n";
+                            texto += nova;
+                        } 
+                        else {
+                            texto += linha + "\n";
+                        }
+                    }
+                    sr.Close();
+                    File.Delete(caminho);
+                    StreamWriter sw = new StreamWriter(caminho);
+                    sw.WriteLine(texto);
+                    sw.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ Aula 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manipula arquivo = new Manipula();
+            Aluno a = new Aluno(6322079, "Ana Beatriz",
+                "3° ADS", "655.432.267-98");
+            arquivo.Gravar(a.RA + ";" + a.Nome + "; " + a.Turma + "; " + a.CPF);
+
+            List<Aluno> alunos = arquivo.Ler();
+            string texto = null;
+            foreach(Aluno fulano in alunos)
+            {
+                texto += "\nRa =" + fulano.RA + " - Nome = " + fulano.Nome + 
+                    " - Curso +" + fulano.Turma + " - CPF =" + fulano.CPF; 
+            }
+            a = new Aluno(6322079, "Ana Beatriz",
+                "3° ADM", "655.432.267-98");
+            arquivo.Alterar(a);
+
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+    }
+}
